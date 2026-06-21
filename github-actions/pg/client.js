422636aea8f2e3ff49d901580a0bfeb46506c70c
Nabil Al Masri <142125053+NabilMx99@users.noreply.github.com>
2026-06-21 20:14:45 +0000

    Update client.js

--- github-actions/pg/client.js
+++ github-actions/pg/client.js
@@ -1,29 +1,52 @@
 const { Client } = require('pg');
 
-const pgclient = new Client({
+async function main() {
+  const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: Number(process.env.POSTGRES_PORT),
     user: 'postgres',
     password: 'postgres',
-    database: 'postgres'
-});
+    database: 'postgres',
+  });
 
-pgclient.connect();
+  try {
+    await pgclient.connect();
 
-const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+    await pgclient.query(`
+      CREATE TABLE IF NOT EXISTS student (
+        id SERIAL PRIMARY KEY,
+        firstName VARCHAR(40) NOT NULL,
+        lastName VARCHAR(40) NOT NULL,
+        age INT,
+        address VARCHAR(80),
+        email VARCHAR(40)
+      )
+    `);
 
-pgclient.query(table, (err, res) => {
-    if (err) throw err
-});
+    const insertText = `
+      INSERT INTO student(firstName, lastName, age, address, email)
+      VALUES($1, $2, $3, $4, $5)
+      RETURNING *
+    `;
+    const values = [
+      'Mona the',
+      'Octocat',
+      9,
+      '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States',
+      'octocat@github.com',
+    ];
 
-pgclient.query(text, values, (err, res) => {
-    if (err) throw err
-});
+    const inserted = await pgclient.query(insertText, values);
+    console.log('Inserted:', inserted.rows[0]);
 
-pgclient.query('SELECT * FROM student', (err, res) => {
-    if (err) throw err
-    console.log(err, res.rows) // Print the data in student table
-    pgclient.end()
-});
+    const result = await pgclient.query('SELECT * FROM student');
+    console.log('All students:', result.rows);
+  } catch (err) {
+    console.error('Database script failed:', err);
+    process.exitCode = 1;
+  } finally {
+    await pgclient.end();
+  }
+}
+
+main();
